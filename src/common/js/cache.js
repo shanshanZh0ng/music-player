735c3ebd463328e3cabc1b116c0d1a848502648a
shanshanZh0ng <1392106876@qq.com>
2019-06-18 18:19:39 +0800

    music

---
+++ src/common/js/cache.js
@@ -0,0 +1,18 @@
+import storage from 'good-storage'
+
+const SEARCH_KEY = '__search__'
+const SEARCH_MAX_LEN = 15
+
+function insertArray(arr, val, compare, maxLen) {
+  const index = arr.findIndex(compare)
+  if (index === 0) {
+    return
+  }
+  if (index > 0) {
+    arr.splice(index, 1)
+  }
+  arr.unshift(val)
+  if (maxLen && arr.length > maxLen) {
+    arr.pop()
+  }
+}
