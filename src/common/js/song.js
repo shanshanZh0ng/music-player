acd8698aea0f5937650fa6fbd4027505ef0c2065
shanshanZh0ng <1392106876@qq.com>
2019-06-20 19:26:47 +0800

    lack of personal center

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -43,7 +43,7 @@ export function createSong(musicData) {
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
     // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
     // url: `http://dl.stream.qqmusic.qq.com/C400003SGnx01dEnNe.m4a?guid=926585190&vkey=C99FD4FF7E632ED884D046E3311A982829569023327B18F2569FF31C280AAE4F383EFB023A68CC69EC5F4E84443483BCD4E446DA3D8B15D0&uin=0&fromtag=38`
-    url: `http://dl.stream.qqmusic.qq.com/C400000H2jfM21Wl7c.m4a?guid=926585190&vkey=0568F7860C97AB3B8091C252549A1C32FD775B3EEAE029148429A354A7F8671C21FBDFB26CC0DED01F1256E5E1E810F201185394BDA6525D&uin=0&fromtag=38`
+    url: `http://dl.stream.qqmusic.qq.com/C400001Z7M2S3GZVB0.m4a?guid=926585190&vkey=7835685A10F0E10306FE34E2B84321FBD48D6BC6F5E23A4DB427DB74688B29180FABAB920D9E23875D3E55CED1814C70F1DD8C99AB67DB24&uin=0&fromtag=38`
   })
 }
 
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -91,30 +91,33 @@
             <i @click.stop="togglePlaying" class="icon-mini" :class="miniIcon"></i>
           </progress-circle>         
         </div>
-        <div class="control">
+        <div class="control" @click.stop="showPlaylist">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <playlist ref="playlist"></playlist>
     <audio ref="audio" :src="currentSong.url" @play="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters, mapMutations} from 'vuex'
+  import {mapGetters, mapMutations, mapActions} from 'vuex'
   import animations from 'create-keyframe-animation'
   import {prefixStyle} from 'common/js/dom'
   import ProgressBar from 'base/progress-bar/progress-bar'
   import ProgressCircle from 'base/progress-circle/progress-circle'
   import {playMode} from 'common/js/config'
-  import {shuffle} from 'common/js/util'
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
+  import Playlist from 'components/playlist/playlist'
+  import {playerMixin} from 'common/js/mixin'
 
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
 
   export default {
+    mixins: [playerMixin],
     data() {
       return {
         songReady: false,
@@ -147,12 +150,8 @@
       },
       ...mapGetters([
         'fullScreen',
-        'playlist',
-        'currentSong',
         'playing',
-        'currentIndex',
-        'mode',
-        'sequenceList'
+        'currentIndex'
       ])
     },
     created() {
@@ -265,6 +264,7 @@
       },
       ready() {
         // this.songReady = true
+        this.savePlayHistory(this.currentSong)
       },
       error() {
         // this.songReady = true
@@ -288,31 +288,12 @@
           this.currentLyric.seek(currentTime * 1000)
         }
       },
-      changeMode() {
-        const mode = (this.mode + 1) % 3
-        this.setPlayMode(mode)
-        let list = null
-        if (mode === playMode.random) {
-          list = shuffle(this.sequenceList)
-        } else {
-          list = this.sequenceList
-        }
-        this.resetCurrentIndex(list)
-        this.setPlaylist(list)
-      },
-      resetCurrentIndex(list) {
-        let index = list.findIndex((item) => {
-          return item.id === this.currentSong.id
-        })
-        this.setCurrentIndex(index)
-      },
       getLyric() {
         this.currentSong.getLyric().then((lyric) => {
           this.currentLyric = new Lyric(lyric, this.handleLyric)
           if (this.playing) {
             this.currentLyric.play()
           }
-          // console.log(this.currentLyric)
         }).catch(() => {
           this.currentLyric = null
           this.playingLyric = ''
@@ -381,6 +362,9 @@
         this.$refs.middleL.style.opacity = opacity
         this.$refs.middleL.style[transitionDuration] = `${time}ms`
       },
+      showPlaylist() {
+        this.$refs.playlist.show()
+      },
       _pad(num, n = 2) {
         let len = num.toString().length
         while (len < n) {
@@ -405,15 +389,17 @@
         }
       },
       ...mapMutations({
-        setFullScreen: 'SET_FULL_SCREEN',
-        setPlayingState: 'SET_PLAYING_STATE',
-        setCurrentIndex: 'SET_CURRENT_INDEX',
-        setPlayMode: 'SET_PLAY_MODE',
-        setPlaylist: 'SET_PLAYLIST'
-      })
+        setFullScreen: 'SET_FULL_SCREEN'
+      }),
+      ...mapActions([
+        'savePlayHistory'
+      ])
     },
     watch: {
       currentSong(newSong, oldSong) {
+        if (!newSong.id) {
+          return
+        }
         if (newSong.id === oldSong.id) {
           return
         }
@@ -435,7 +421,8 @@
     components: {
       ProgressBar,
       ProgressCircle,
-      Scroll
+      Scroll,
+      Playlist
     }
   }
 </script>
